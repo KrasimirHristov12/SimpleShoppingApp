70183ea70b1c6d7ae7a1fb2fae0e6f8fd63204c1
KrasimirHristov12 <hkrasimir53@gmail.com>
2024-01-21 17:30:27 +0200

    Worked on UI

---
+++ SimpleShoppingApp/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleShoppingApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("/[controller]/{id}")]
+        public IActionResult Index(int id)
+        {
+            // Get product from the db
+
+            // Transform it to vm & pass it to the view
+
+            return View();
+        }
+
+        [Route("/[controller]/[action]/{name}")]
+        public IActionResult Category(string name)
+        {
+            // Get category products - Get count, Implement Paging, filters (like in Emag)
+
+            return View();
+        }
+    }
+}
