2c2e2189724a26eb8e7db6f6a47e17fd6a4d5cbf
KrasimirHristov12 <hkrasimir53@gmail.com>
2024-03-13 15:08:38 +0200

    A few tweaks on validation and moved order summary to a partial view

--- SimpleShoppingApp.Web/wwwroot/js/order-validate.js
+++ SimpleShoppingApp.Web/wwwroot/js/order-validate.js
@@ -1,5 +1,5 @@
 ﻿const inputs = $(".quantity-input");
-
+const form = $("#order-form");
 const validateObject = {
     rules: {
 
@@ -16,47 +16,57 @@ for (let i = 0; i < inputs.length; i++) {
     }
 }
 
+form.validate(validateObject);
 
-$("#order-form").validate(validateObject);
 
 $(".quantity-input").on("keyup change", function () {
-    let quantityInput = $(this)
-    let updatedQuantity = quantityInput.val();
-    let productName = quantityInput.closest(".row").find("p strong").eq(0).text();
-    if (!isNaN(updatedQuantity)) {
-        let updatedQuantityNum = parseInt(updatedQuantity);
-        if (updatedQuantityNum > 0) {
-            let idOfProduct = quantityInput.closest(".product").find("input:hidden").val();
-            $.ajax({
-                type: "POST",
-                url: "/Cart/UpdateQuantity",
-                data: {
-                    productId: idOfProduct,
-                    updatedQuantity: updatedQuantityNum.toString(),
-                },
-                success: function (data) {
-                    if (data.updatedQuantity == 0) {
-                        alert(`Unfortunately, ${productName} is out of stock. It will be deleted from your cart.`);
-                        quantityInput.closest(".row").find(".remove-btn").click();
-                    }
-                    else {
-                        let quantityLeft = data.updatedQuantity;
-                        if (data.isThereLessThanRequested) {
-                            alert(`There are ${quantityLeft} ${productName}s left.`);
+
+    if (!form.valid()) {
+        $(".total-price").text("");
+        const notHiddenErrors = $("label.error").not(":hidden");
+        notHiddenErrors.each(function () {
+            $(this).closest(".row").find(".product-price").text("");
+        });
+    }
+
+    else {
+
+        let quantityInput = $(this)
+        let updatedQuantity = quantityInput.val();
+        let productName = quantityInput.closest(".row").find("p strong").eq(0).text();
+        if (!isNaN(updatedQuantity)) {
+            let updatedQuantityNum = parseInt(updatedQuantity);
+            if (updatedQuantityNum > 0) {
+                let idOfProduct = quantityInput.closest(".product").find("input:hidden").val();
+                $.ajax({
+                    type: "POST",
+                    url: "/Cart/UpdateQuantity",
+                    data: {
+                        productId: idOfProduct,
+                        updatedQuantity: updatedQuantityNum.toString(),
+                    },
+                    success: function (data) {
+                        if (data.updatedQuantity == 0) {
+                            alert(`Unfortunately, ${productName} is out of stock. It will be deleted from your cart.`);
+                            quantityInput.closest(".row").find(".remove-btn").click();
                         }
-                        
-                        quantityInput.val(quantityLeft);
-                        quantityInput.closest("div.mb-4").next().find(".product-price").text('$' + parseFloat(data.newProductPrice).toFixed(2));
-                        $(".total-price").text('$' + parseFloat(data.newTotalPrice).toFixed(2));
-                    }
-                    
-                },
-                dataType: "json",
-            });
+                        else {
+                            let quantityLeft = data.updatedQuantity;
+                            if (data.isThereLessThanRequested) {
+                                alert(`There are ${quantityLeft} ${productName}s left.`);
+                            }
+
+                            quantityInput.val(quantityLeft);
+                            quantityInput.closest("div.mb-4").next().find(".product-price").text('$' + parseFloat(data.newProductPrice).toFixed(2));
+                            $(".total-price").text('$' + parseFloat(data.newTotalPrice).toFixed(2));
+                        }
+
+                    },
+                    dataType: "json",
+                });
+
+            }
         }
     }
-    else {
-        alert("Product quantity must be a number");
-        $(this).val("1");
-    }
+
 });
