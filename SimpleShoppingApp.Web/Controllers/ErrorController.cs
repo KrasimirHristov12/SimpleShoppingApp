a9c642e8f5d611794ee410c2a6be3db9047e6e5e
KrasimirHristov12 <hkrasimir53@gmail.com>
2024-03-24 18:11:23 +0200

    Implemented status code pages

---
+++ SimpleShoppingApp.Web/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleShoppingApp.Models;
+using System.Diagnostics;
+
+namespace SimpleShoppingApp.Web.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        [AllowAnonymous]
+        [ActionName("404")]
+        public IActionResult NotFoundError() 
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ActionName("500")]
+        public IActionResult ServerError()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [AllowAnonymous]
+        [ActionName("400")]
+        public IActionResult BadRequestError()
+        {
+            return View();
+        }
+    }
+}
--- SimpleShoppingApp.Web/Program.cs
+++ SimpleShoppingApp.Web/Program.cs
@@ -75,11 +75,12 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
